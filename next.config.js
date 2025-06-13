321c2f88f45092479cd062b0b724394052f963e7
niushiqi <719586684@qq.com>
2025-06-13 15:58:05 +0800

    修改部署文件

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // 启用静态导出
+  distDir: "out",    // 输出目录（默认是 "out"）
+  trailingSlash: true,  // 可选：添加尾部斜杠
   images: {
     domains: ['images.unsplash.com', 'miro.medium.com', 'www.anseladams.com', 'upload.wikimedia.org', 'www.moma.org', 'www.magnumphotos.com'],
     formats: ['image/avif', 'image/webp'],
